270e26cd7ccb07feb1da870779e23d7284c75541
natalia3101 <118992516+natalia3101@users.noreply.github.com>
2023-10-12 20:58:55 +0300

    add lesson2

---
+++ lesson1/demo/src/main/java/task5/MVSCalculator.java
@@ -0,0 +1,22 @@
+package task5;
+
+public class MVSCalculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+        
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+        
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+        
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+}
---
+++ lesson1/demo/src/main/java/task5/MathUtils.java
@@ -0,0 +1,9 @@
+package task5;
+
+public class MathUtils {
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
